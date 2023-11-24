336ef87a6e0aaa155f0242721d1300d7a4411dcb
makgadk2@gmail.com <makgadk2@gmail.com>
2023-11-24 05:09:37 +0200

    Very first documentation

---
+++ src/main/java/com/zadscience/modern/order/fulfilment/neworderfulfilment/NewOrderFulfilmentApplication.java
@@ -0,0 +1,13 @@
+package com.zadscience.modern.order.fulfilment.neworderfulfilment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewOrderFulfilmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewOrderFulfilmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zadscience/modern/order/fulfilment/neworderfulfilment/NewOrderFulfilmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zadscience.modern.order.fulfilment.neworderfulfilment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewOrderFulfilmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
