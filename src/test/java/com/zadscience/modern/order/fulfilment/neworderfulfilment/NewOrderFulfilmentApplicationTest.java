c7b544281f1bd52f2c90dbc9f47afcaf8fef76e3
makgadk2@gmail.com <makgadk2@gmail.com>
2023-12-20 07:06:51 +0200

    Added a post test for Order Transfer

--- src/test/java/com/zadscience/modern/order/fulfilment/neworderfulfilment/NewOrderFulfilmentApplicationTest.java
+++ src/test/java/com/zadscience/modern/order/fulfilment/neworderfulfilment/NewOrderFulfilmentApplicationTest.java
@@ -4,6 +4,9 @@ package com.zadscience.modern.order.fulfilment.neworderfulfilment;
 import com.zadscience.modern.order.fulfilment.neworderfulfilment.controller.NewOrderFulfilmentController;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.nullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.zadscience.modern.order.fulfilment.neworderfulfilment.entities.Customer;
 import com.zadscience.modern.order.fulfilment.neworderfulfilment.repository.NewOrderFulfilmentRepository;
@@ -33,6 +36,27 @@ public class NewOrderFulfilmentApplicationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private  String orderTransferRequest = """
+                {
+                   "transferringCustomer":{
+                      "customerId":2000445,
+                      "name":"James",
+                      "surname":"Sanders",
+                      "address":"44 Brio Avenue, Cape Town, 30009",
+                      "externalOrder":{
+                         "orderNumber":1022334,
+                         "orderDate":"03-10-2022",
+                         "trackingNumber":"CompetitorRef::12345-2000445",
+                         "externalProduct":null,
+                         "id":null,
+                         "externalOrderReference":"CompetitorRef::1234567-2000445"
+                      }
+                   },
+                   "electableCustomerReference":"12345",
+                   "transferringCustomerReference":"CompetitorRef::2000445"
+                }
+                """;
+
     @Test
     void testController() throws Exception {
         assertThat(controller).isNotNull();
@@ -41,6 +65,25 @@ public class NewOrderFulfilmentApplicationTest {
     @Test
     void testNewOrderFulfilmentRepository() throws Exception {
         assertThat(newOrderFulfilmentRepository).isNotNull();
+    }
+
+    @Test
+    public void testOrderTransfer()
+    {
+        try {
+            mockMvc.perform(post("/api/v1/fulfilment/transfer/order")
+                            .contentType("application/json")
+                            .content(orderTransferRequest))
+                    .andExpect(status().isCreated());
+
+            ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/fulfilment/" +
+                    "/show/electable/customers"));
+            //After transferring an order from James (E-comms Env 1) to Thandi ( E-comms Env 2) Thandi must now have 2 orders.
+            response.andExpect(MockMvcResultMatchers.jsonPath("$[0].orders", hasSize(2)));
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
@@ -54,12 +97,19 @@ public class NewOrderFulfilmentApplicationTest {
                 "/show/electable/customers"));
 
         // then - verify the output
-        response.andExpect(MockMvcResultMatchers.status().isOk());
+        response.andExpect(status().isOk());
         response.andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(customers.size())));
         response.andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1))); //check if the array size is 1
         response.andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Thandi")); //check if the array size is 1
+        response.andExpect(MockMvcResultMatchers.jsonPath("$[0].orders", hasSize(1))); //check if the array size is 1
+        response.andExpect(MockMvcResultMatchers.jsonPath("$[0].externalOrder").value(nullValue()));
+
 
     }
+
+
+
+
 
 
 
